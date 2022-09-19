2b17871b8224c7a9fbc9de44f44959e9342fdfc5
nacho <hjoseigancio@gmail.com>
2022-09-19 12:51:51 +0200

    add changes to E2E demo

--- python_aef/aef_to_capif.py
+++ python_aef/aef_to_capif.py
@@ -9,39 +9,10 @@ import redis
 REDIS_HOST = os.getenv('REDIS_HOST')
 REDIS_PORT = os.environ.get('REDIS_PORT')
 
-from OpenSSL.SSL import FILETYPE_PEM
-from OpenSSL.crypto import (dump_certificate_request, dump_privatekey, load_publickey, PKey, TYPE_RSA, X509Req, dump_publickey)
-
-
-def create_csr(csr_file_path):
-    private_key_path = "private.key"
-
-    # create public/private key
-    key = PKey()
-    key.generate_key(TYPE_RSA, 2048)
-
-    # Generate CSR
-    req = X509Req()
-    req.get_subject().CN = 'exposer'
-    req.get_subject().O = 'Telefonica I+D'
-    req.get_subject().OU = 'Innovation'
-    req.get_subject().L = 'Madrid'
-    req.get_subject().ST = 'Madrid'
-    req.get_subject().C = 'ES'
-    req.get_subject().emailAddress = 'inno@tid.es'
-    req.set_pubkey(key)
-    req.sign(key, 'sha256')
-
-    with open(csr_file_path, 'wb+') as f:
-        f.write(dump_certificate_request(FILETYPE_PEM, req))
-        csr_request = dump_certificate_request(FILETYPE_PEM, req)
-    with open(private_key_path, 'wb+') as f:
-        f.write(dump_privatekey(FILETYPE_PEM, key))
-
-    return csr_request
-
 
 def register_exposer_to_capif(capif_ip, capif_port, username, password, role, description, cn):
+
+    print("Registering exposer to CAPIF")
     url = "http://{}:{}/register".format(capif_ip, capif_port)
 
     payload = dict()
@@ -56,16 +27,30 @@ def register_exposer_to_capif(capif_ip, capif_port, username, password, role, de
     }
 
     try:
+        print("''''''''''REQUEST'''''''''''''''''")
+        print("Request: to ",url) 
+        print("Request Headers: ",  headers) 
+        print("Request Body: ", json.dumps(payload))
+        print("''''''''''REQUEST'''''''''''''''''")
         response = requests.request("POST", url, headers=headers, data=json.dumps(payload))
         response.raise_for_status()
         response_payload = json.loads(response.text)
+        print("''''''''''RESPONSE'''''''''''''''''")
+        print("Response to: ",response.url) 
+        print("Response Headers: ",  response.headers) 
+        print("Response: ", response.json())
+        print("Response Status code: ", response.status_code)
+        print("Success to register new exposer")
+        print("''''''''''RESPONSE'''''''''''''''''")
         return response_payload['id'], response_payload['ccf_publish_url']
     except requests.exceptions.HTTPError as err:
         raise Exception(err.response.text, err.response.status_code)
 
 
-def get_capif_token(capif_ip, capif_port, username, password, role):
-    url = "http://{}:{}/gettoken".format(capif_ip, capif_port)
+def get_capif_auth(capif_ip, capif_port, username, password, role):
+
+    print("Geting Auth to exposer")
+    url = "http://{}:{}/getauth".format(capif_ip, capif_port)
 
     payload = dict()
     payload['username'] = username
@@ -77,41 +62,73 @@ def get_capif_token(capif_ip, capif_port, username, password, role):
     }
 
     try:
+        print("''''''''''REQUEST'''''''''''''''''")
+        print("Request: to ",url) 
+        print("Request Headers: ",  headers) 
+        print("Request Body: ", json.dumps(payload))
+        print("''''''''''REQUEST'''''''''''''''''")
+
         response = requests.request("POST", url, headers=headers, data=json.dumps(payload))
+
         response.raise_for_status()
         response_payload = json.loads(response.text)
-        return response_payload['access_token']
+        certification_file = open('exposer.crt', 'wb+')
+        private_key_file = open("private.key", 'wb+')
+        certification_file.write(bytes(response_payload['cert'], 'utf-8'))
+        private_key_file.write(bytes(response_payload['private_key'], 'utf-8'))
+        certification_file.close()
+        private_key_file.close()
+
+        print("''''''''''RESPONSE'''''''''''''''''")
+        print("Response to: ",response.url) 
+        print("Response Headers: ",  response.headers) 
+        print("Response: ", response.json())
+        print("Response Status code: ", response.status_code)
+        print("Get AUTH Success. Created private key and cert file ")
+        print("''''''''''RESPONSE'''''''''''''''''")
+        return
     except requests.exceptions.HTTPError as err:
         raise Exception(err.response.text, err.response.status_code)
 
+def register_api_provider_to_capif(capif_ip, ccf_url):
 
-def publish_service_api_to_capif(capif_ip, jwt_token, ccf_url):
+    print("Registering api provider to CAPIF")
 
-    csr_request = create_csr("cert_req.csr")
-
-    url = 'http://{}:8080/sign-csr'.format(capif_ip)
-
-    payload_dict = dict()
-    payload_dict['csr'] = csr_request.decode("utf-8")
-    payload_dict['mode'] = 'client'
-    payload_dict['filename'] = 'exposer'
-    payload = json.dumps(payload_dict)
-
+    url = 'https://{}/{}'.format(capif_ip, ccf_url)
+    payload = open('api_provider_domain.json', 'rb')
     headers = {
         'Content-Type': 'application/json'
     }
 
-    certification_file = open('exposer.crt', 'wb')
     try:
-        response = requests.request("POST", url, headers=headers, data=payload)
+
+        print("''''''''''REQUEST'''''''''''''''''")
+        print("Request: to ",url) 
+        print("Request Headers: ",  headers) 
+        print("Request Body: ", json.dumps(payload))
+        print("''''''''''REQUEST'''''''''''''''''")
+
+        response = requests.request("POST", url, headers=headers, data=payload, cert=('exposer.crt', 'private.key'), verify='ca.crt')
         response.raise_for_status()
         response_payload = json.loads(response.text)
-        print(response_payload['certificate'])
-        certification_file.write(bytes(response_payload['certificate'], 'utf-8'))
-    except requests.exceptions.HTTPError as err:
-        raise Exception(err.response.text, err.response.status_code)
 
-    certification_file.close()
+        print("''''''''''RESPONSE'''''''''''''''''")
+        print("Response to: ",response.url) 
+        print("Response Headers: ",  response.headers) 
+        print("Response: ", response.json())
+        print("Response Status code: ", response.status_code)
+        print("Success, registered api provider domain to CAPIF")
+        print("''''''''''RESPONSE'''''''''''''''''")
+        return response_payload['apiId']
+    except requests.exceptions.HTTPError as err:
+        message = json.loads(err.response.text)
+        status = err.response.status_code
+        raise Exception(message, status)
+
+
+def publish_service_api_to_capif(capif_ip, ccf_url):
+
+    print("Publishing api service to CAPIF")
 
     url = 'https://{}/{}'.format(capif_ip, ccf_url)
     payload = open('service_api_description.json', 'rb')
@@ -120,9 +137,23 @@ def publish_service_api_to_capif(capif_ip, jwt_token, ccf_url):
     }
 
     try:
+        print("''''''''''REQUEST'''''''''''''''''")
+        print("Request: to ",url) 
+        print("Request Headers: ",  headers) 
+        #print("Request Body: ", payload)
+        print("''''''''''REQUEST'''''''''''''''''")
+
         response = requests.request("POST", url, headers=headers, data=payload, cert=('exposer.crt', 'private.key'), verify='ca.crt')
         response.raise_for_status()
         response_payload = json.loads(response.text)
+
+        print("''''''''''RESPONSE'''''''''''''''''")
+        print("Response to: ",response.url) 
+        print("Response Headers: ",  response.headers) 
+        print("Response: ", response.json())
+        print("Response Status code: ", response.status_code)
+        print("Success, registered api service to CAPIF")
+        print("''''''''''RESPONSE'''''''''''''''''")
         return response_payload['apiId']
     except requests.exceptions.HTTPError as err:
         message = json.loads(err.response.text)
@@ -146,13 +177,13 @@ if __name__ == '__main__':
     role = config.get("credentials", "exposer_role")
     description = config.get("credentials", "exposer_description")
     cn = config.get("credentials", "exposer_cn")
-
+    
     capif_ip = config.get("credentials", "capif_ip")
     # capif_port = config.get("credentials", "capif_port")
     #capif_ip = os.getenv('CAPIF_HOSTNAME')
     capif_port = os.getenv('CAPIF_PORT')
     
-
+    #First we need register exposer in CAPIF
     try:
         if not r.exists('exposerID'):
             exposerID, ccf_publish_url = register_exposer_to_capif(capif_ip, capif_port, username, password, role, description,cn)
@@ -166,25 +197,25 @@ if __name__ == '__main__':
         else:
             print(e)
 
+    #Second, we need get auth, in this case create cert and private key file
     try:
-        if not r.exists('capif_access_token_exposer'):
-            capif_access_token = get_capif_token(capif_ip, capif_port, username, password, role)
-            r.set('capif_access_token_exposer', capif_access_token)
-            print("Capif Token: {}".format(capif_access_token))
+        get_capif_auth(capif_ip, capif_port, username, password, role)
+
     except Exception as e:
         status_code = e.args[0]
         if status_code == 401:
-            print("Bad credentials. User not found\n")
+            print("Bad credential or User not found\n")
         else:
             print(e)
         capif_access_token = None
 
+
+    #Third publish service in CAPIF
     try:
         if r.exists('exposerID'):
-            # exposerID = r.get('exposerID')
             ccf_publish_url = r.get('ccf_publish_url')
             capif_access_token = r.get('capif_access_token_exposer')
-            service_api_id = publish_service_api_to_capif(capif_ip, capif_access_token, ccf_publish_url)
+            service_api_id = publish_service_api_to_capif(capif_ip, ccf_publish_url)
             if not r.exists('services_num'):
                 services_num = 0
             else:
@@ -199,7 +230,7 @@ if __name__ == '__main__':
         if status_code == 401:
             message = e.args[0]
             if str(message).find("Token has expired") != -1:
-                capif_access_token = get_capif_token(capif_ip, capif_port, username, password, role)
+                capif_access_token = get_capif_auth(capif_ip, capif_port, username, password, role)
                 r.set('capif_access_token_exposer', capif_access_token)
                 print("New Capif Token: {}".format(capif_access_token))
                 print("Run the script again to publish a Service API")
