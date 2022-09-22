d46714146e69e9319501add5f7b930f47b540e30
nacho <hjoseigancio@gmail.com>
2022-09-22 08:13:59 +0200

    add env variables to prepare script

--- python_aef/prepare.sh
+++ python_aef/prepare.sh
@@ -1,13 +1,9 @@
-
-
-echo "$CAPIF_HOSTNAME"
-
 COUNTRY="ES"                # 2 letter country-code
 STATE="Madrid"            # state or province name
 LOCALITY="Madrid"        # Locality Name (e.g. city)
 ORGNAME="Telefonica I+D" # Organization Name (eg, company)
 ORGUNIT="Innovation"                  # Organizational Unit Name (eg. section)
-COMMONNAME="$CAPIF_HOSTNAME"
+COMMONNAME="python-aef"
 EMAIL="inno@tid.es"    # certificate's email address
 # optional extra details
 CHALLENGE=""                # challenge password
@@ -29,7 +25,11 @@ $CHALLENGE
 $COMPANY
 __EOF__
 
-curl --request GET 'http://$CAPIF_HOSTNAME:$CAPIF_PORT/ca-root' 2>/dev/null | jq -r '.certificate' -j > ca.crt
+curl  --connect-timeout 5 \
+    --max-time 10 \
+    --retry-delay 0 \
+    --retry-max-time 40 \
+    --request GET "http://$CAPIF_HOSTNAME:$CAPIF_PORT/ca-root" 2>/dev/null | jq -r '.certificate' -j > ca.crt
 
 echo '172.17.0.1      capifcore' >> /etc/hosts
 
