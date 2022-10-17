af23e9b9996f042f461aa1e9b2adcf50b6aa5d2a
raxonros <hjoseigancio@gmail.com>
2022-10-17 11:59:58 +0200

    Capif aef demo (#1)
    * add gitignore
    
    * add changes to E2E demo
    
    * update gitignore
    
    * add path to ca
    
    * separe functions in diferent files
    
    * Uncomment get env variable for CAPIF_HOST
    
    * create service certificate
    
    * fix error
    
    * add env variables to prepare script
    
    * add order name to python files
    
    * remove underscore
    
    * change name file
    
    * change mayus Exposer to exposer
    
    * delete keys from redis
    
    * modify cn
    
    * add colours
    
    * Bug fix: ca root not taken on start up, change order of commands in prepare.sh
    
    * Deploy callback servers, specify name for redis to distinguish, small fixes
    
    Co-authored-by: Jorge Moratinos Salcines <jorge.moratinossalcines@telefonica.com>
    Co-authored-by: Stavros Charismiadis <stauros411@gmail.com>

--- cleanup_docker_containers.sh
+++ cleanup_docker_containers.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+sudo rm ./python_aef/*.crt ./python_aef/*.key
+
 docker-compose down --rmi all --remove-orphans || true
--- python_aef/prepare.sh
+++ python_aef/prepare.sh
@@ -1,5 +1,36 @@
-curl --request GET 'http://host.docker.internal:8080/ca-root' 2>/dev/null | jq -r '.certificate' -j > ca.crt
+COUNTRY="ES"                # 2 letter country-code
+STATE="Madrid"            # state or province name
+LOCALITY="Madrid"        # Locality Name (e.g. city)
+ORGNAME="Telefonica I+D" # Organization Name (eg, company)
+ORGUNIT="Innovation"                  # Organizational Unit Name (eg. section)
+COMMONNAME="python_aef"
+EMAIL="inno@tid.es"    # certificate's email address
+# optional extra details
+CHALLENGE=""                # challenge password
+COMPANY=""                  # company name
+
+# DAYS="-days 365"
+
+# create the certificate request
+#cat <<__EOF__ | openssl req -new $DAYS -nodes -keyout client.key -out client.csr
+cat <<__EOF__ | openssl req -newkey rsa:4096 -nodes -sha256 -keyout domain.key -x509 -days 365 -out domain.crt
+$COUNTRY
+$STATE
+$LOCALITY
+$ORGNAME
+$ORGUNIT
+$COMMONNAME
+$EMAIL
+$CHALLENGE
+$COMPANY
+__EOF__
 
 echo '172.17.0.1      capifcore' >> /etc/hosts
+
+curl  --connect-timeout 5 \
+    --max-time 10 \
+    --retry-delay 0 \
+    --retry-max-time 40 \
+    --request GET "http://${CAPIF_HOSTNAME}:${CAPIF_PORT}/ca-root" 2>/dev/null | jq -r '.certificate' -j > ca.crt
 
 tail -f /dev/null
